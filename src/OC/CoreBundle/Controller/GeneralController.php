cecbd05f5f250dce0e7b82112be48ff778fe7c39
gilles81 <gilles.sabathe@free.fr>
2018-11-16 23:05:00 +0100

    work for flash

--- src/OC/CoreBundle/Controller/GeneralController.php
+++ src/OC/CoreBundle/Controller/GeneralController.php
@@ -2,7 +2,10 @@
 
 namespace OC\CoreBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class GeneralController extends Controller
 {
@@ -32,13 +35,27 @@ class GeneralController extends Controller
           'date'    => new \Datetime())
       );
         return $this->render('OCCoreBundle:General:Home.html.twig',array(
-          'listAdverts' => $listAdverts,
-        ));
+          'listAdverts' => $listAdverts));
     }
 
-    public function ContactAction()
+    public function ContactAction(Request $request)
     {
-      return $this->render('OCCoreBundle:General:Contact.html.twig');
+      $session = $request->getSession();
+
+      // Bien sûr, cette méthode devra réellement ajouter l'annonce
+
+      // Mais faisons comme si c'était le cas
+      $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+
+      // Le « flashBag » est ce qui contient les messages flash dans la session
+      // Il peut bien sûr contenir plusieurs messages :
+      $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+
+      // Puis on redirige vers la page de visualisation de cette annonce
+      return $this->redirectToRoute('oc_homepage' );
+
     }
+
+
 
 }
