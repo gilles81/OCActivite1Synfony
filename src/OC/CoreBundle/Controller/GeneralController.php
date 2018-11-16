90c088902269ec938091538f091eb6f90ab023ed
gilles81 <gilles.sabathe@free.fr>
2018-11-16 15:33:48 +0100

    Page d'accueil en cours de creation + menu contact

--- src/OC/CoreBundle/Controller/GeneralController.php
+++ src/OC/CoreBundle/Controller/GeneralController.php
@@ -8,6 +8,31 @@ class GeneralController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('OCCoreBundle:Index:Layout.html.twig');
+
+
+      // Notre liste d'annonce en dur
+      $listAdverts = array(
+        array(
+          'title'   => 'Recherche développpeur Symfony',
+          'id'      => 1,
+          'author'  => 'Alexandre',
+          'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+          'date'    => new \Datetime()),
+        array(
+          'title'   => 'Mission de webmaster',
+          'id'      => 2,
+          'author'  => 'Hugo',
+          'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+          'date'    => new \Datetime()),
+        array(
+          'title'   => 'Offre de stage webdesigner',
+          'id'      => 3,
+          'author'  => 'Mathieu',
+          'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+          'date'    => new \Datetime())
+      );
+        return $this->render('OCCoreBundle:Home:Home.html.twig',array(
+          'listAdverts' => $listAdverts,
+        ));
     }
 }
